9a81ee0bee8a34a6766ed16e543fa8d08563cfcd
Salvialf <jeuxalf@live.fr>
2020-11-24 14:54:33 +0100

    V4 update

--- desktop/php/caldav.php
+++ desktop/php/caldav.php
@@ -2,137 +2,151 @@
 if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
-sendVarToJS('eqType', 'caldav');
-$eqLogics = eqLogic::byType('caldav');
+$plugin = plugin::byId('caldav');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un agenda}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
-                foreach ($eqLogics as $eqLogic) {
-                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-                }
-                ?>
-            </ul>
-        </div>
-    </div>
-    
-     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-        <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-        <div class="eqLogicThumbnailContainer">
-            <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-                <center>
-                    <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
-                </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-            </div>
-            <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-                <center>
-                <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-                </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
-            </div>
-        </div>
-        <legend>{{Mes agendas Caldav}}
-        </legend>
-		<div class="eqLogicThumbnailContainer">
-			  <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-				 <center>
-					<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-				</center>
-				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-			</div>
-			<?php
-			if (count($eqLogics) == 0) {
-				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>Vous n'avez encore d'agenda Caldav, cliquez à gauche sur le bouton ajouter un agenda pour commencer</span></center>";
-			} else {
-				foreach ($eqLogics as $eqLogic) {
-					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-					echo "<center>";
-					echo '<img src="plugins/caldav/doc/images/caldav_icon.png" height="105" width="95" />';
-					echo "</center>";
-					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-					echo '</div>';
-				}
-			}
-			?>
-		</div>
-    </div>
+     <div class="col-xs-12 eqLogicThumbnailDisplay">
+       <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+   		<div class="eqLogicThumbnailContainer">
+   			<div class="cursor eqLogicAction logoPrimary" data-action="add">
+   				<i class="fas fa-plus-circle"></i>
+   				<br>
+   				<span>{{Ajouter}}</span>
+   			</div>
+   			<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+   				<i class="fas fa-wrench"></i>
+   				<br>
+   				<span>{{Configuration}}</span>
+   			</div>
+   		</div>
+        <legend><i class='icon divers-calendar2'></i> {{Mes agendas Caldav}}</legend>
+      <div class="input-group" style="margin:5px;">
+  			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>
+  			<div class="input-group-btn">
+  				<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
+  			</div>
+  		</div>
+  		<!-- Liste des équipements du plugin -->
+  		<div class="eqLogicThumbnailContainer">
+  			<?php
+  			foreach ($eqLogics as $eqLogic) {
+  				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+  				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+  				echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+  				echo '<br>';
+  				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+  				echo '</div>';
+  			}
+  			?>
+  		</div>
+  	</div> <!-- /.eqLogicThumbnailDisplay -->
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-        <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-        </ul>
-        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+    <div class="col-xs-12 eqLogic" style="display: none;">
+  		<!-- barre de gestion de l'équipement -->
+  		<div class="input-group pull-right" style="display:inline-flex;">
+  			<span class="input-group-btn">
+  				<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces entre les boutons. Ne pas modifier -->
+  				<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
+  				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
+  				</a><a class="btn btn-sm btn-danger eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}
+  				</a>
+  			</span>
+  		</div>
+  		<!-- Onglets -->
+  		<ul class="nav nav-tabs" role="tablist">
+  			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+  			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+  			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+  		</ul>
+  		<div class="tab-content">
+  			<!-- Onglet de configuration de l'équipement -->
+  			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
+  				<br/>
+  				<div class="row">
+  					<div class="col-lg-7">
+  						<form class="form-horizontal">
+  							<fieldset>
+  								<legend><i class="fas fa-wrench"></i> {{Général}}</legend>
+  								<div class="form-group">
+  									<label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
+  									<div class="col-xs-11 col-sm-7">
+  										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;"/>
+  										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+  									</div>
+  								</div>
+  								<div class="form-group">
+  									<label class="col-sm-3 control-label" >{{Objet parent}}</label>
+  									<div class="col-xs-11 col-sm-7">
+  										<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+  											<option value="">{{Aucun}}</option>
+  											<?php
+  											$options = '';
+  											foreach ((jeeObject::buildTree(null, false)) as $object) {
+  												$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
+  											}
+  											echo $options;
+  											?>
+  										</select>
+  									</div>
+  								</div>
+  								<div class="form-group">
+  									<label class="col-sm-3 control-label">{{Catégorie}}</label>
+  									<div class="col-sm-9">
+  										<?php
+  										foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+  											echo '<label class="checkbox-inline">';
+  											echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+  											echo '</label>';
+  										}
+  										?>
+  									</div>
+  								</div>
+  								<div class="form-group">
+  									<label class="col-sm-3 control-label">{{Options}}</label>
+  									<div class="col-xs-11 col-sm-7">
+  										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+  										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+  									</div>
+  								</div>
+
+  								<br/>
+  								<legend><i class="fas fa-cogs"></i> {{Paramètres}}</legend>
                         <div class="form-group">
-                            <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
-                            <div class="col-sm-2">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement caldav"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-                            <div class="col-sm-2">
-                                <select class="eqLogicAttr form-control" data-l1key="object_id">
-                                    <option value="">{{Aucun}}</option>
-                                    <?php
-                                    foreach (object::all() as $object) {
-                                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                          <label class="col-sm-2 control-label" ></label>
-                            <div class="col-sm-10">
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">{{URL caldav}}</label>
-                            <div class="col-lg-4">
+                            <label class="col-sm-3 control-label">{{URL caldav}}</label>
+                            <div class="col-xs-11 col-sm-7">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="url"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-2 control-label">{{Compte}}</label>
-                            <div class="col-lg-2">
+                            <label class="col-sm-3 control-label">{{Nom d'utilisateur}}</label>
+                            <div class="col-xs-11 col-sm-7">
                                 <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="username"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-2 control-label">{{Password}}</label>
-                            <div class="col-lg-2">
+                            <label class="col-sm-3 control-label">{{Mot de passe}}</label>
+                            <div class="col-xs-11 col-sm-7">
                                 <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-2 control-label">{{Calendrier}}</label>
-                            <div class="col-lg-3">
+                            <label class="col-sm-3 control-label">{{Calendrier}}</label>
+                            <div class="col-xs-11 col-sm-7">
                                 <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="calendrier" id="CalendarsList">
                                 </select>
                             </div>
                         </div>
-                    </fieldset> 
+                    </fieldset>
                 </form>
            </div>
+      </div>
+           </div>
             <div role="tabpanel" class="tab-pane" id="commandtab">
-                <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une pattern caldav}}</a><br/><br/>
+              <a class="btn btn-default btn-sm pull-right cmdAction" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une pattern caldav}}</a>
+              <br/><br/>
                 <table id="table_cmd" class="table table-bordered table-condensed">
                     <thead>
                         <tr>
